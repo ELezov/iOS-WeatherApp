90e7572c31f8b6e4a698f640916fed6cb530e2a8
Lezov Eugene <el@appkode.ru>
2018-01-23 17:00:59 +0200

    Добавил extension для UIColor и UIViewController

---
+++ WeatherApp/common/Extensions/UIViewController.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewController.swift
+//  WeatherApp
+//
+//  Created by KODE-i7-1 on 23/01/2018.
+//  Copyright © 2018 KODE-i7-1. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
